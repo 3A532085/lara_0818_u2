7032b1414ef1e5c45b9dee57b7e1e37e57d519b5
江珮妤 <Luxgen077@gmail.com>
2018-11-09 15:34:13 +0800

    練習2:建立樣板結構

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title') | My Blog</title>
+</head>
+<body>
+@include('layouts.partials.navigation')
+<div class="container-fluid">
+    @yield('content')
+</div>
+</body>
+</html>
